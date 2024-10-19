b9c5184d610baef623d8d2334e2b0daa1a8cb6dd
piseb <piseb@tuta.com>
2024-10-19 14:28:36 +0200

    API is now available in https

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiURL: "https://jo24-back-dev-lb-1658764817.eu-west-3.elb.amazonaws.com/api/v1/"
+  apiURL: "https://api.jo24-demo.link/api/v1/"
 };
